63d0c442d2daad35b082ee700c80c8d17c323a6c
lany159 <tnfl2644@gmail.com>
2024-03-25 17:07:20 +0900

    basic1-1

--- app/src/main/java/com/example/basic1/Text1Activity.kt
+++ app/src/main/java/com/example/basic1/Text1Activity.kt
@@ -15,8 +15,7 @@ import kotlinx.coroutines.Job
 
 class Text1Activity : AppCompatActivity() {
     private var job: Job? = null
-
-    //    val randomValue = (1..100).random()
+    val randomValue = (1..100).random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test1)
@@ -29,18 +28,13 @@ class Text1Activity : AppCompatActivity() {
     private fun setupButton() {
         val button = findViewById<Button>(R.id.clickButton)
         button.setOnClickListener {
-            Log.d("activity", "stop")
             job?.cancel()
-            checkAnswerAndShowToast()
         }
     }
 
     private fun setRandomValueBetweenOneToHundred() {
         val randomTextView = findViewById<TextView>(R.id.textViewRandom)
-        val randomValue = (1..100).random()
         randomTextView.setText(randomValue.toString())
-//      randomTextView.text = randomValue.toString()
-//      setText > EditText(입력받는 위젯)에 쓰이는 함수
     }
 
     private fun setJobAndLaunch() {
@@ -50,10 +44,10 @@ class Text1Activity : AppCompatActivity() {
             for (i in 1..100) {
                 if (isActive) {
                     textView.text = i.toString()
-//                    if (i == randomValue) {
-//                        toast()
-//                        break
-//                    }
+                    if (i == randomValue) {
+                        toast()
+                        break
+                    }
                     delay(500)
                 }
             }
@@ -61,16 +55,7 @@ class Text1Activity : AppCompatActivity() {
         }
     }
 
-//    private fun toast() {
-//        Toast.makeText(this, "같은 값 입니다", Toast.LENGTH_SHORT).show()
-//    }
-
-    private fun checkAnswerAndShowToast() {
-        val textView = findViewById<TextView>(R.id.spartaTextView)
-        val randomTextView = findViewById<TextView>(R.id.textViewRandom)
-        if (textView.text.toString().toInt() == randomTextView.text.toString().toInt()) {
-            Toast.makeText(this, "같은 값 입니다", Toast.LENGTH_SHORT).show()
-        }
-        Toast.makeText(this,"같은 값이 아닙니다",Toast.LENGTH_SHORT).show()
+    private fun toast() {
+        Toast.makeText(this, "같은 값 입니다", Toast.LENGTH_SHORT).show()
     }
 }
